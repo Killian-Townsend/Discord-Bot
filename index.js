ba94826992b0103663350b0069356483e8e9c009
kittypickles9982 <oasisscript87@gmail.com>
2020-09-02 13:37:52 -0400

    Updated index.js to include error catching

--- index.js
+++ index.js
@@ -18,40 +18,67 @@ for (const file of commandFiles) {
 }
 
 client.once('ready', () => {
-	console.log('Bot Loading...'.brightMagenta);
-	console.log(`Repair Mode : ${repair}`.brightMagenta);
-	console.log(`Command Kill : ${cmd_kill}`.brightMagenta);
-	console.log(`Prefix Set : ${prefix}`.brightMagenta);
-	const curWatch = Math.floor(Math.random() * watchCount);
-	const watch = watchList[curWatch];
-	console.log(`Bot Activity is : Playing A Game ${watch}`.brightMagenta);
-	client.user.setActivity(watch);
-	console.log('Bot Activity Set'.brightMagenta);
-	console.log(`cmd_ch : ${cmd_ch}`.brightMagenta);
-	console.log('Bot Ready!'.brightGreen);
-	console.log('‎‎‎‎‎‎‎‎‏‏‎            ‎‏'.black);
-	client.channels.cache.get(mod_ch).send('Bot Ready!');
-	return (console.error);
+	
+	try {
+		console.log('[SYS] Bot Loading...'.brightMagenta);
+		console.log(`[SYS] Repair Mode : ${repair}`.brightMagenta);
+		console.log(`[SYS] Command Kill : ${cmd_kill}`.brightMagenta);
+		console.log(`[SYS] Prefix Set : ${prefix}`.brightMagenta);
+		const curWatch = Math.floor(Math.random() * watchCount);
+		const watch = watchList[curWatch];
+		console.log(`[SYS] Bot Activity is : Playing A Game ${watch}`.brightMagenta);
+		client.user.setActivity(watch);
+		console.log('[SYS] Bot Activity Set'.brightMagenta);
+		console.log(`[SYS] cmd_ch : ${cmd_ch}`.brightMagenta);
+		console.log('[INFO] Bot Ready!'.brightGreen);
+		console.log('‎‎‎‎‎‎‎‎‏‏‎            ‎‏'.black);
+		client.channels.cache.get(mod_ch).send('Bot Ready!');
+		return (console.error);
+	} catch (error) {
+		console.log(`[FATAL] Loading Error : ${error}`.red.bold);
+		process.exit();
+	}
 	
 });	
 
 client.on ("guildMemberAdd", member => {
 	
-	
 	try {
-		member.send(`Hello ${member.user.username}! Welcome to **Centripetal Force**! I am <@743834091333484625>, Centripetal Force's custom bot! Written by kittypickles9982! Make sure to read over the quick rules, roles, and punishments to see if this server is right for you! When you've made your decision, go ahead and react to Carl's message. You'll then gain access to the rest of the server! If you have *any* questions, feel free to DM an online mod, we'll probably get back to you soon! Hope to see you inside ${member.user.username}!`);
-	} catch (error) {
-		console.log(`[ERROR] Unable To DM Member ${member.user.username}!`.red.bold);
-	}
+		if (member.user.id === '187355156793393152') {
+			try {
+				member.send(`${member.user.username}, you have been permanently banned from **Centripetal Force**. You may **not** appeal this ban.`);
+				setTimeout(function(){
+					member.ban();
+				}, 500);
+			} catch (error) {
+				console.log(`[ERROR] Error Banning Member : ${error}`.brightRed.bold);
+			}
+		
+		} else {
 	
+			try {
+				member.send(`Hello ${member.user.username}! Welcome to **Centripetal Force**! I am <@743834091333484625>, Centripetal Force's custom bot! Written by kittypickles9982! Make sure to read over the quick rules, roles, and punishments to see if this server is right for you! When you've made your decision, go ahead and react to Carl's message. You'll then gain access to the rest of the server! If you have *any* questions, feel free to DM an online mod, we'll probably get back to you soon! Hope to see you inside ${member.user.username}!`);
+				client.channels.cache.get('748907768492327018').send(`Welcome ${member.user.username}! Welcome to **Centripetal Force**`);
+			} catch (error) {
+				console.log(`[ERROR] Unable To DM Member ${member.user.username} : ${error}`.brightRed.bold);
+			}
+		}
+	} catch (error) {
+		console.log(`[ERROR] An Error Occured While Adding A Member : ${error}`.brightRed.bold);
+	}
 })
 
 client.on('message', message => {
 	
-	const members = client.guilds.cache.get('748907767661592596').memberCount;
-
-	message.guild.channels.cache.get('749276654165164073').setName(`Members : ${members}`);
+	try {
 	
+	const members = client.guilds.cache.get('748907767661592596').memberCount - 8;
+	
+	client.channels.cache.get('749276654165164073').setName(`Members : ${members}`);
+	
+	} catch (error) {
+		console.log(`[ERROR] Member Update Error : ${error}`.red.bold);
+	}
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 	if (message.channel.type === 'dm') return;
 	if (cmd_ch == undefined || message.channel.id === cmd_ch) {
@@ -69,15 +96,20 @@ client.on('message', message => {
 					client.commands.get(command).execute(message, args,addons);
 				} catch (error) {
 					message.channel.send(`Unknown Command, Try Using ${prefix}help`);
+					console.log(`[WARNING] Unknown CMD or Broken CMD : ${error}`.brightYellow.bold);
 				}
 			}	
-		const dateObj = new Date(comSend); 
-		const hours = dateObj.getUTCHours() - 4; 
-		const minutes = dateObj.getUTCMinutes(); 
-		const seconds = dateObj.getUTCSeconds(); 
-		const comSendForm = hours.toString().padStart(2, '0') + ':' + minutes.toString().padStart(2, '0') + ':' + seconds.toString().padStart(2, '0'); 
-		const comTaken = Date.now() - comSend;
-		console.log(`${comSendForm}  |  Command Sent : ${command}  |  Latency : ${timeTaken}ms  |  Time Taken : ${comTaken}ms`.brightCyan);
+		try {
+			const dateObj = new Date(comSend); 
+			const hours = dateObj.getUTCHours() - 4; 
+			const minutes = dateObj.getUTCMinutes(); 
+			const seconds = dateObj.getUTCSeconds(); 
+			const comSendForm = hours.toString().padStart(2, '0') + ':' + minutes.toString().padStart(2, '0') + ':' + seconds.toString().padStart(2, '0'); 
+			const comTaken = Date.now() - comSend;
+			console.log(`[CMD] ${comSendForm}  |  Command Sent : ${command}  |  Latency : ${timeTaken}ms`.brightCyan);
+		} catch (error) {
+			console.log(`[ERROR] Time Function Error : ${error}`.brightRed.bold);
+		}
 	} else {
 		return;
 	}
