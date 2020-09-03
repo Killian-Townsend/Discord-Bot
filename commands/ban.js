77e4e2651e189b631f8da9ff9e723d5f510e87a7
kittypickles9982 <oasisscript87@gmail.com>
2020-09-03 14:52:00 -0400

    Added Ban Command - Broken

--- commands/ban.js
+++ commands/ban.js
@@ -1,8 +1,63 @@
 module.exports = {
 	name: 'ban',
-	description: 'Replies With Bot Latency',
-	execute(message, args) {
-		const timeTaken = Date.now() - message.createdTimestamp;
-		message.channel.send(`Latency : ${timeTaken}ms`);
+	description: 'Bans User Mention - Syntax : ban @USER reason',
+	execute(message, args, addons) {
+
+		try {
+
+		    const ops = addons[4];
+		    const mod_ch = addons[6];
+		    const guild_id = addons[7];
+		    const client = addons[3];
+		    const ls = ops.includes(message.author.tag);
+		    const ban_mem = args[0];
+		    args.shift();
+            var ban_reason;
+
+		    if (!!args) {
+		        var ban_reason = "No Reason Given";
+		    } else {
+		        var ban_reason = args.join(' ');
+            }
+		    if (ls === true) {
+                if (ban_mem == undefined) {
+                    message.channel.send('Please mention a user');
+                } else {
+                    const is_men = ban_mem.startsWith('<@!');
+                    if (is_men === true) {
+
+                            const ban_mem_id = ban_mem.replace(/[\\<>@#&!]/g, "");
+                            const ban_mem_name = message.guild.members.cache.get(ban_mem_id).username;
+
+                            const avatar = client.guilds.resolve(guild_id).members.resolve(ban_mem_id).user.displayAvatarURL({ format: 'png', dynamic: false, size: 1024 });
+
+                            const ban_embed = {
+                                color: 0xd10700,
+                                title: `${ban_mem_name} Was Banned`,
+                                thumbnail: avatar,
+                                fields: [
+                                    {
+                                        name: 'Ban Reason',
+                                        value: `${ban_reason}`,
+                                        inline: true,
+                                    }
+                                ],
+                                timestamp: new Date()
+                            };
+
+                            client.channels.cache.get(mod_ch).send({ embed: ban_embed });
+
+                            member.ban(ban_reason);
+
+                    } else {
+                        message.channel.send('Please mention a user');
+                    }
+                }
+            } else {
+            	message.channel.send('You Lack The Permissions To Use This Command');
+            }
+		} catch (error) {
+			console.log(`[WARNING] : Problem Banning Member : ${error}`.yellow.bold);
+		}
 	},
 };
